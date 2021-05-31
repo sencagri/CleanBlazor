a0dcecf8ed135dc2161bcfcb05d23c1cf2330291
sencagri <ebubekircagrisen@gmail.com>
2021-05-31 11:24:58 +0300

    Initial commit

---
+++ BlazorHero.CleanArchitecture/Server/Controllers/v1/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BlazorHero.CleanArchitecture.Application.Features.Dashboards.Queries.GetData;
+using BlazorHero.CleanArchitecture.Shared.Constants.Permission;
+
+namespace BlazorHero.CleanArchitecture.Server.Controllers.v1
+{
+    [ApiController]
+    public class DashboardController : BaseApiController<DashboardController>
+    {
+        [Authorize(Policy = Permissions.Dashboards.View)]
+        [HttpGet]
+        public async Task<IActionResult> GetDataAsync()
+        {
+            var result = await _mediator.Send(new GetDashboardDataQuery());
+            return Ok(result);
+        }
+    }
+}
